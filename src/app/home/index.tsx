370bbef3eac27c09cabaf21671eb091e01d1eea0
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2023-09-10 12:24:08 -0300

    style:Adds images files

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,22 +1,3 @@
-import Image from 'next/image'
-import bg from '../../../public/imgs/homeBGImg.png'
-
-export function Home() {
-  return (
-    <div className="overflow-hidden ">
-      <div className="bg-pink relative w-screen h-screen">
-        <Image
-          src={bg}
-          style={{ objectFit: 'cover' }}
-          fill
-          alt=""
-          objectPosition="top"
-          className="opacity-40"
-          placeholder="blur"
-        />
-
-        {/* <div className="absolute inset-0 bg-gradient-to-r from-pink to-transparent z-10 w-[1980px]" /> */}
-      </div>
-    </div>
-  )
+export default function Home() {
+  return <div>Home</div>
 }
